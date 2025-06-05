011e1fd1be7ea9b15db923077881193ec2068b43
github 250450031 <taisei-akamatsu@kccs.co.jp>
2025-06-05 09:23:10 +0900

    初回コミット

---
+++ Java/src/Java.java
@@ -0,0 +1,14 @@
+
+
+public class Java {
+   public static void main(String[] args) {
+      int n = 50;
+      while( -1 ){
+         System.out.print(n);
+         if(n == 45){
+                  break;
+         }
+         --n;
+      }
+   }
+}
